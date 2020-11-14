f5ee213d681947affad47bd04361fc941cbd77fe
Lim Geun <shing100@naver.com>
2020-11-14 16:52:57 +0900

    회원 가입 폼 서브밋 처리

--- src/main/java/com/kingname/study/account/AccountController.java
+++ src/main/java/com/kingname/study/account/AccountController.java
@@ -1,6 +1,9 @@
 package com.kingname.study.account;
 
+import com.kingname.study.domain.Account;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,6 +19,8 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -34,12 +39,25 @@ public class AccountController {
             return "account/sign-up";
         }
 
-//        signUpFormValidator.validate(signUpForm, errors);
-//        if (errors.hasErrors()) {
-//            return "account/sign-up";
-//        }
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) // TODO 인코딩
+                .studyEnrollmentResultByWeb(true)
+                .studyCreatedByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
 
-        // TODO 회원 가입 처리
+        Account newAccount = accountRepository.save(account);
+
+        newAccount.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("가입하신 스터디, 회원 가입 인증");
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
+                        "&email=" + newAccount.getEmail());
+        javaMailSender.send(mailMessage);
+
         return "redireckt:/";
     }
 }
