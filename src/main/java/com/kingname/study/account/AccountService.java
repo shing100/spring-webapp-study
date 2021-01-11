6e8b3418953ecf77f1b231afb0d46d3dd2fcd847
Lim Geun <shing100@naver.com>
2021-01-11 23:50:00 +0900

    feat 이메일로 비밀번호 찾기 기능 추가

--- src/main/java/com/kingname/study/account/AccountService.java
+++ src/main/java/com/kingname/study/account/AccountService.java
@@ -108,4 +108,14 @@ public class AccountService implements UserDetailsService {
         accountRepository.save(account);
         login(account);
     }
+
+    public void sendLoginLink(Account account) {
+        account.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(account.getEmail());
+        mailMessage.setSubject("스터디, 로그인 링크");
+        mailMessage.setText("/login-by-email?token=" + account.getEmailCheckToken() +
+                "&email=" + account.getEmail());
+        javaMailSender.send(mailMessage);
+    }
 }
