c42ab9e2cfe4021b7846f91a9d7cbd5245ac2966
Lim Geun <shing100@naver.com>
2021-01-19 21:04:26 +0900

    feat 유저별 tag 검색 기능 추가

--- src/main/java/com/kingname/study/account/AccountService.java
+++ src/main/java/com/kingname/study/account/AccountService.java
@@ -21,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -126,5 +127,10 @@ public class AccountService implements UserDetailsService {
         byId.ifPresent(a -> a.getTags().add(tag));
 
         // accountRepository.getOne() LazyLoding
+    }
+
+    public Set<Tag> getTags(Account account) {
+        Optional<Account> byId = accountRepository.findById(account.getId());
+        return byId.orElseThrow().getTags();
     }
 }
