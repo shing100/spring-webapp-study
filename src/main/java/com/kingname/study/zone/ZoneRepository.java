6eef8263267c05da3461aa13df86b19c05a1eef3
Lim Geun <shing100@naver.com>
2021-01-28 20:23:58 +0900

    feat 지역 리스트 추가

---
+++ src/main/java/com/kingname/study/zone/ZoneRepository.java
@@ -0,0 +1,8 @@
+package com.kingname.study.zone;
+
+import com.kingname.study.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+
+}
