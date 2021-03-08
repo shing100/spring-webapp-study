287d341a847df8ea2e6b3d7b6b000a26c625e53e
Lim Geun <shing100@naver.com>
2021-03-08 23:16:13 +0900

    feat Event, Enrollment 도메인 추가

---
+++ src/main/java/com/kingname/study/domain/Enrollment.java
@@ -0,0 +1,29 @@
+package com.kingname.study.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@EqualsAndHashCode(of = "id")
+public class Enrollment {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Event event;
+
+    @ManyToOne
+    private Account account;
+
+    private LocalDateTime enrolledAt;
+
+    private boolean accepted;
+
+    private boolean attended;
+}
---
+++ src/main/java/com/kingname/study/domain/EventType.java
@@ -0,0 +1,6 @@
+package com.kingname.study.domain;
+
+public enum EventType {
+
+    FCFS, CONFIRMATIVE;
+}
