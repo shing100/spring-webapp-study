a4aad66a4e72e5a2000bda277f01cb9cab211509
Lim Geun <shing100@naver.com>
2021-05-16 14:20:48 +0900

    검색 기능 구현

--- src/main/java/com/kingname/study/modules/main/MainController.java
+++ src/main/java/com/kingname/study/modules/main/MainController.java
@@ -2,12 +2,20 @@ package com.kingname.study.modules.main;
 
 import com.kingname.study.modules.account.CurrentAccount;
 import com.kingname.study.modules.account.Account;
+import com.kingname.study.modules.study.Study;
+import com.kingname.study.modules.study.StudyRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final StudyRepository studyRepository;
 
     @GetMapping("/")
     public String home(@CurrentAccount Account account, Model model) {
@@ -21,5 +29,13 @@ public class MainController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/search/study")
+    public String searchStudy(String keyword, Model model) {
+        List<Study> studyList = studyRepository.findByKeyword(keyword);
+        model.addAttribute(studyList);
+        model.addAttribute("keyword", keyword);
+        return "search";
     }
 }
