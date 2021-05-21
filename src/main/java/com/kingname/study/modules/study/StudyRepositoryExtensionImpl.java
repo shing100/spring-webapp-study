deec7af784a4f25ba0b8c112fd690c098a899462
Lim Geun <shing100@naver.com>
2021-05-21 23:00:47 +0900

    fix N+1 Select 문제 해결

--- src/main/java/com/kingname/study/modules/study/StudyRepositoryExtensionImpl.java
+++ src/main/java/com/kingname/study/modules/study/StudyRepositoryExtensionImpl.java
@@ -1,5 +1,8 @@
 package com.kingname.study.modules.study;
 
+import com.kingname.study.modules.account.QAccount;
+import com.kingname.study.modules.tag.QTag;
+import com.kingname.study.modules.zone.QZone;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
@@ -17,7 +20,11 @@ public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport impl
         JPQLQuery<Study> query = from(study).where(study.published.isTrue()
                 .and(study.title.containsIgnoreCase(keyword))
                 .or(study.tags.any().title.containsIgnoreCase(keyword))
-                .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)));
+                .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)))
+                .leftJoin(study.tags, QTag.tag).fetchJoin()
+                .leftJoin(study.zones, QZone.zone).fetchJoin()
+                .leftJoin(study.members, QAccount.account).fetchJoin()
+                .distinct();
         return query.fetch();
     }
 }
